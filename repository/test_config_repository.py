b10d8bb4929586731fee360601261211c95621ec
michihiro169 <michihiro169@gmail.com>
2025-03-20 12:14:28 +0900

    feat: リポジトリ追加

---
+++ repository/test_config_repository.py
@@ -0,0 +1,30 @@
+import yaml
+from src.test.case.test_case import TestCase
+from src.test.config.test_config import TestConfig
+from src.test.object.part.test_object_part import TestObjectPart
+from src.test.perspective.test_perspective import TestPerspective
+
+class TestConfigRepository():
+    def getTestConfig(self) -> TestConfig:
+        data = {}
+        with open("./storage/test_config/共通.yml", 'r') as file:
+            data = yaml.safe_load(file)
+
+        perspectives = []
+        for perspectiveName in data:
+            cases = []
+            for case in data[perspectiveName]:
+                cases.append(TestCase(
+                    case['パターン'],
+                    case['手順'],
+                    case['想定結果']
+                ))
+            perspectives.append(TestPerspective(perspectiveName, cases))
+        objectPart = TestObjectPart("共通", perspectives)
+
+        # テスト観点
+        perspectives = None
+        with open("./storage/test_config/テスト観点.yml", 'r') as file:
+            perspectives = yaml.safe_load(file)
+
+        return TestConfig(objectPart, perspectives)
